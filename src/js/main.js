98f2a082ac79cc173b4faa9b7aaf7c88f427f588
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2024-02-15 22:26:09 +0330

    add: landing

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+let sidebar = document.querySelector('#sidebar');
+
+const openSidebar = () => sidebar?.classList.toggle("active");
+
+
+const closeSidebar = () => sidebar?.classList.remove("active");
+
+
+const articleDropdown = () => {
+  const dropdownMenu = document.querySelector("#dropdownMenu");
+  const icon = document.querySelector('.rotate-icon');
+  icon.classList.toggle('active');
+  dropdownMenu.classList.toggle("active");
+  if (dropdownMenu.classList.contains("active")) {
+    dropdownMenu.style.height = dropdownMenu.scrollHeight + "px";
+  } else {
+    dropdownMenu.style.height = "0";
+    setTimeout(() => {
+      dropdownMenu.style.height = "";
+    }, 300);
+  }
+};
+
